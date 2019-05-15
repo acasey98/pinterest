c14ad2f55a94e8f552d305b85a4d45e6e3c4990b
Austin Casey <thepoast@gmail.com>
2019-05-14 19:24:30 -0500

    printdom

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,23 @@
 import boardData from '../../helpers/data/boardsData';
+import util from '../../helpers/util';
+
+const domStringBuilder = (boardsArray) => {
+  let domString = '';
+  boardsArray.forEach((board) => {
+    domString += `<div id=${board.id} class="card col-3" style="width: 18rem;">`;
+    domString += '<div class="card-body">';
+    domString += `<p class="card-text">${board.name}<p>`;
+    domString += '</div>';
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
   boardData.loadBoards()
     .then((resp) => {
-      console.error('resp', resp.data.boards);
+      const boardsResults = resp.data.boards;
+      domStringBuilder(boardsResults);
     })
     .catch(err => console.error('error from loadBoards', err));
 };
